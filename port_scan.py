3f9e970f8d28dd85d8465e01c5330aab5c5e6a15
Mrityunjay Dev Gangwar <mrityunjaygangwar834@gmail.com>
2026-06-18 06:52:00 +0530

    Improve readability and structure of port_scan.py
    Refactor port scan script for clarity and consistency.

--- port_scan.py
+++ port_scan.py
@@ -1,35 +1,34 @@
 import socket
 
-# 1. Setup target details
-TARGET_IP = "192.168.56.101"  # <-- Double check your Kali IP here
-TARGET_PORT = 8000             # This matches the Netcat port we opened
 
-# 2. Create a standard TCP Socket
-# AF_INET means IPv4, SOCK_STREAM means TCP
+TARGET_IP = "192.168.56.101" 
+TARGET_PORT = 8000             
+
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.settimeout(3.0)  # Stop waiting after 3 seconds if port is dead
+client_socket.settimeout(3.0)  
 
 try:
     print(f"Attempting a Connect Scan on {TARGET_IP}:{TARGET_PORT}...")
     
-    # 3. Attempt the standard OS TCP 3-Way Handshake
+    
     client_socket.connect((TARGET_IP, TARGET_PORT))
     print("[SUCCESS] Port Detection: Port is OPEN!")
     
-    # --- NEW: BANNER GRABBING PIECE ---
-    print("Sending HTTP request to grab service banner...")
     
-    # Construct a raw HTTP GET request string
+    print("Sending HTTP request to grab service banner")
+    
+  
     http_request = b"GET / HTTP/1.1\r\nHost: 192.168.56.101\r\n\r\n"
     client_socket.send(http_request)
     
-    # Read the first 1024 bytes of the server's response
+    
     response = client_socket.recv(1024).decode(errors='ignore')
     
-    print("\n--- GRABBED BANNER INFORMATION ---")
+    print("\n GRABBED BANNER INFORMATION")
     print(response)
     print("----------------------------------")
-    # ----------------------------------
+
     
 
 except socket.timeout:
@@ -37,5 +36,5 @@ except socket.timeout:
 except ConnectionRefusedError:
     print("[CLOSED] Port Detection: Port is CLOSED!")
 finally:
-    # 4. Clean up the connection
+    
     client_socket.close()
